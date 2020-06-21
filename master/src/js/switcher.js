a08c289f480a8d791ca70c19deb231654eb1f232
Nick Snell <nicksnell@pm.me>
2020-06-21 20:54:06 +0100

    fix: use switcher on master site; set path on cookie to root

--- master/src/js/switcher.js
+++ master/src/js/switcher.js
@@ -1,20 +1,23 @@
 var regions = ["de", "gb", "ie"];
 
 function setRegion(region) {
-  document.cookie = "nf_country=" + region;
+  document.cookie = "nf_country=" + region + ";path=/";
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   var output = document.createElement("p");
+  output.className = "text-center font-semibold text-gray-700 text-lg";
 
   for (var i = 0; i < regions.length; i++) {
     var region = regions[i];
     var link = document.createElement("a");
-    link.className = "pr-2";
+    link.className = "pr-2 underline";
     link.innerHTML = region;
+    link.href = "#";
     link.onclick = function () {
       setRegion(region);
       window.location = "/";
+      return false;
     };
     output.appendChild(link);
   }
